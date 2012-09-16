de75422868cac1fce26b07f8b5a96ab257a0499f
Tushar Pankaj <tushar.s.pankaj@gmail.com>
2012-09-15 17:13:54 -0700

    Temporary tex2pix change
    Signed-off-by: Tushar Pankaj <tushar.s.pankaj@gmail.com>

--- app.py
+++ app.py
@@ -1,10 +1,25 @@
 import os
+import tex2pix
 from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
+    document = """
+\documentclass{article}
+\usepackage[margin=1in]{geometry}
+\usepackage{amsmath}
+\usepackage{graphicx}
+\graphicspath{{/tmp/}}
+\begin{document}
+
+What is the minimum number of small squares that must be colored black so that a line of symmetry lies on the diagonal $ \overline{BD}$ of square $ ABCD$? (2008 AMC 8 \#3) \\
+\includegraphics[height=2.5cm]{2005AMC8n3.png}
+\end{document}
+"""
+    renderer = tex2pix.Renderer(document)
+    renderer.mkpdf('/tmp/output.pdf')
     return render_template('index.html')
 
 if __name__ == '__main__':
