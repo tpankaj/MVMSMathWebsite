eaefdc9dd8688b8f122ae296a31c835e43f9ce31
Tushar Pankaj <tushar.s.pankaj@gmail.com>
2012-09-24 12:12:03 -0700

    Added pages
    Signed-off-by: Tushar Pankaj <tushar.s.pankaj@gmail.com>

--- app.py
+++ app.py
@@ -1,11 +1,27 @@
 import os
-from flask import Flask, render_template
+from flask import Flask, Markup, render_template, abort
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('index.html', current='index')
+    return render_template('index.html', current='index', user='Tushar Pankaj')
+
+@app.route('/<page>/')
+def go_to(page):
+    page = linkname(page)
+    pages = list()
+    pages.append('about')
+    pages.append('contact')
+    for pageiterator in pages:
+        if page == pageiterator:
+            return render_template('pages/%s.html' % page, current=page, user='Tushar Pankaj')
+    abort(404)
+
+def linkname(title):
+  title = title.strip()
+  title = ''.join(c for c in title if c.isalnum() or c.isspace() or c == '-')
+  return title.lower().replace(' ', '-')
 
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
